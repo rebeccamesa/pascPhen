e31aab1bdf9cf90adf29cb049b6096b05eacef16
Rebecca Mesa <rebecca.mesa01@universitadipavia.it>
2022-02-01 19:15:56 +0100

    Update to distribution

--- R/mlho_features.R
+++ R/mlho_features.R
@@ -1,7 +1,7 @@
 #' Apply MLHO to extract features
-#' 
+#'
 #' @importFrom data.table ":="
-#' 
+#'
 #' @param PatientObservations 2.1 PatientObservations table
 #' @param PatientSummary 2.1 PatientSummary table
 #' @param rules table containing the phenotype definitions
@@ -9,14 +9,14 @@
 #' @param days.pre "pre" stage threshold
 #' @param phenotype phenotype label
 #' @param prediction do you want to make prediction or phenotyping?
-#' @param test.sample mlho.it parameter. Percentage for testing 
+#' @param test.sample mlho.it parameter. Percentage for testing
 #' @param MSMR.sparsity MSMR.lite parameter
 #' @param MSMR.topn MSMR.lite parameter
 #' @param multicore do you want to parallelize the process?
 #' @param iteration mlho.it parameter. Number of iterations
 #' @param score.threshold MLHO score threshold
 #' @param classifier mlearn parameter. Classification algorithm
-#' 
+#'
 #' @return table containing the extracted features with their ORs
 #' @export
 
@@ -35,37 +35,39 @@ mlho_features <- function(PatientObservations,
                           score.threshold=80,
                           classifier)
 {
-  
+
   # Prepare labeldt, dbmart and dems
-  
+
   rules.phen <- dplyr::filter(rules, PASC_Phenotype == phenotype)
-  
+
   PatientObservations <- dplyr::mutate(PatientObservations, patient_num=as.character(patient_num))
-  PatientSummary <- dplyr::mutate(PatientSummary, age_group = dplyr::case_when((age_group == "80plus" | age_group == "70to79") ~ "70plus", 
-                                                                                     TRUE ~ "12to69")) #let's binarize age
+
+  # Let's binarize age. "other" has to be discussed
+  PatientSummary <- dplyr::mutate(PatientSummary, age_group = dplyr::case_when((age_group == "80plus" | age_group == "70to79") ~ "70plus",
+                                                                                     TRUE ~ "00to69"))
   labeldt <- createLabels(days.long, rules.phen, PatientObservations)
-  
+
   dems <- PatientSummary %>%
     dplyr::select(patient_num, sex, age_group)%>%
     dplyr::mutate(age_group = as.factor(age_group), sex = as.factor(sex))
-  
+
   PatientObservations.excl <- PatientObservations %>%
     dplyr::filter(!(paste(gsub(".*-", "", PatientObservations$concept_type), PatientObservations$concept_code) %in% paste(rules.phen$Coding, rules.phen$Code)))%>% # let's exclude codes used to define the phenotype
     dplyr::mutate(phenx = paste(concept_type,concept_code, sep = "_"))
-  
+
   if (prediction) {
     dbmart <- dplyr::filter(PatientObservations.excl, days_since_admission < days.pre) #prediction
   } else {
     dbmart <- dplyr::filter(PatientObservations.excl, days_since_admission > days.long) #phenotyping
   }
   dbmart <- dplyr::mutate(dbmart[,c('patient_num', 'phenx')], DESCRIPTION = phenx)
-  
+
   # Implement iterative MLHO
-  
+
   data.table::setDT(dbmart)
   dbmart[,row :=.I]
   dbmart$value.var <- 1
-  
+
   mlho.features <- mlho::mlho.it(dbmart,
                            labels = labeldt,
                            dems,
@@ -76,20 +78,20 @@ mlho_features <- function(PatientObservations,
                            mlearn.aoi=phenotype,
                            multicore=multicore,
                            iterations=iteration)
-  
+
   # Compute MLHO confidence scores (CS)
-  
+
   mlho.scores <- mlho::mlho.cs(mlho.features)
   mlho.features <- c(as.character(subset(mlho.scores$features,mlho.scores$cs >= score.threshold)))
-  
+
   # Implement mlearn with no test set with a training set made of the entire population of MSMR.lite
-  
+
   dbmart <- subset(dbmart,dbmart$phenx %in% mlho.features)
   setDT(dbmart)
   dbmart[,row := .I]
   dbmart$value.var <- 1
   uniqpats <- c(as.character(unique(dbmart$patient_num)))
-  
+
   model.data <- mlho::MSMSR.lite(MLHO.dat=dbmart,patients = uniqpats,sparsity=NA,jmi = FALSE,labels = labeldt)
   model.output <- mlho::mlearn(dat.train=model.data,
                        dat.test=NULL,
@@ -99,9 +101,9 @@ mlho_features <- function(PatientObservations,
                        note="extracting_coefficients",
                        aoi=phenotype,
                        multicore=multicore)
-  
+
   model.output <- stats::na.omit(model.output)
-  
+
   return(model.output)
 }
 
--- R/phen_distribution.R
+++ R/phen_distribution.R
@@ -21,16 +21,16 @@ phen_distribution <- function(PatientObservations,
   PatientObservations <- dplyr::select(PatientObservations, -value,-siteid)
 
   data <- PatientSummary %>%
-    dplyr::select(patient_num, sex, admission_date) %>%
+    dplyr::select(patient_num, severe, deceased, sex, age_group, race, admission_date) %>%
     merge(PatientObservations, by = "patient_num")
   data$calendar_date <- as.Date(data$admission_date)+data$days_since_admission
   data <- data %>%
-    dplyr::mutate(stage = dplyr::case_when(days_since_admission > 90 ~ "long",
-                             days_since_admission > 30 ~ "post",
-                             days_since_admission > -14 ~ "acute",
-                             TRUE ~ 'pre'))%>%
-    dplyr::select(c(1,2,3,4,7,8,5,6)) %>%
-    dplyr::mutate(stage = factor(stage, levels = c("pre", "acute", "post", "long")))
+    dplyr::mutate(stage = dplyr::case_when(days_since_admission >= 90 ~ "day90plus",
+                             days_since_admission >= 30 ~ "day30to89",
+                             days_since_admission >= 0 ~ "day0to29",
+                             days_since_admission >=-14 ~ "dayN14toN1",
+                             TRUE ~ "before_adm"))%>%
+    dplyr::mutate(stage = factor(stage, levels = c("before_adm", "dayN14toN1", "day0to29", "day30to89", "day90plus")))
 
   # Compute occurences
 
@@ -47,45 +47,41 @@ phen_distribution <- function(PatientObservations,
       Phen.data.tot<-rbind(Phen.data.tot, Phen.data)
     }
   }
-  names(tot.count)<- c("Phenotype", "pre", "acute", "post", "long")
+  names(tot.count)<- c("Phenotype", "before_adm", "dayN14toN1", "day0to29", "day30to89", "day90plus")
+  tot.count<-tot.count[order(tot.count$Phenotype),]
 
   # Ridgeline plot
 
-  pl1<-ggplot2::ggplot(Phen.data.tot, aes(y = forcats::fct_rev(forcats::fct_infreq(phenotype)), x = as.numeric(days_since_admission), fill = stat(x))) +
-    ggridges::geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, alpha = 0.9) +
+  pl1<-ggplot2::ggplot(Phen.data.tot, aes(y = forcats::fct_rev(phenotype), x = as.numeric(days_since_admission), fill = stat(x))) +
+    ggridges::geom_density_ridges_gradient(scale = 2, rel_min_height = 0.01, alpha = 0.8) +
     ggplot2::theme_minimal()+
     ggplot2::scale_fill_viridis_c(name = "Days since admission:",option = "G")+
     ggplot2::labs(x = "Days since admission", y = "", title = "Phenotype distribution")
 
-  if(!is.null(stratified_by)){
-    pl1<-pl1+
-      ggplot2::facet_wrap(~Phen.data.tot[,stratified_by])+
-      ggplot2::theme_grey()
-  }
-
   # Bar plot
 
-  Phen.data.tot<-dplyr::mutate(Phen.data.tot, stage = factor(stage, levels = c("long", "post", "acute", "pre")))
+  Phen.data.tot<-dplyr::mutate(Phen.data.tot, stage = factor(stage, levels = c("day90plus", "day30to89", "day0to29", "dayN14toN1", "before_adm")))
   if (is.null(stratified_by)) {
-    pl2<-ggplot2::ggplot(Phen.data.tot, aes(forcats::fct_rev(forcats::fct_infreq(phenotype)), fill = stage))+
+    pl2<-ggplot2::ggplot(Phen.data.tot, aes(forcats::fct_rev(phenotype), fill = stage))+
       ggplot2::geom_bar()+
       ggplot2::theme_minimal()+
       ggplot2::coord_flip()+
-      ggplot2::scale_fill_brewer(name = "Stage:", labels = c("Long (90, )", "Post (30, 90]", "Acute (-14, 30]", "Pre ( ,-14]"))+
+      ggplot2::scale_fill_brewer(name = "Stage:", labels = c("day90plus", "day30to89", "day0to29", "dayN14toN1", "before_adm"))+
       ggplot2::labs(y = "Counts", x = "", title = "Phenotype occurences", )
   } else {
-    pl2<-ggplot2::ggplot(Phen.data.tot, aes(y=Phen.data.tot[,stratified_by], fill = stage))+
+    pl2<-ggplot2::ggplot(Phen.data.tot, aes(y=Phen.data.tot[,stratified_by], fill = stage, col=sex))+
         ggplot2::geom_bar()+
-        ggplot2::facet_grid(forcats::fct_infreq(phenotype)~ ., switch = "y")+
+        ggplot2::facet_grid(phenotype~ ., switch = "y")+
         ggplot2::theme_minimal()+
         ggplot2::scale_y_discrete(position = "right")+
-        ggplot2::scale_fill_brewer(name = "Stage:", labels = c("Long (90, )", "Post (30, 90]", "Acute (-14, 30]", "Pre ( ,-14]"))+
+        ggplot2::scale_fill_brewer(name = "Stage:", labels = c("day90plus", "day30to89", "day0to29", "dayN14toN1", "before_adm"))+
         ggplot2::theme(strip.text.y.left = ggplot2::element_text(angle = 0))+
         ggplot2::labs(y = "", x = "Counts", title = paste("Phenotype occurences stratified by",stratified_by))
 
   }
 
   return(list(
+    tot.count = tot.count,
     phen.distributionPlot = pl1,
     phen.barPlot = pl2)
   )
